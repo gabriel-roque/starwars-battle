e701e80584c3c0976c6a23e6373807a978cff387
gabriel-roque <gabrielrms.dll@gmail.com>
2020-01-23 13:31:04 -0300

    feat: add home page star wars

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./assets/css/global.css";
 
 import RouterView from "routes";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -6,11 +7,26 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "store";
 
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+
 export default function App() {
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: "dark",
+      primary: {
+        main: "#ffc501",
+        dark: "#65ff00"
+      }
+    }
+  });
+
   return (
     <Provider store={store}>
       <Router>
-        <RouterView />
+        <ThemeProvider theme={darkTheme}>
+          <RouterView />
+        </ThemeProvider>
       </Router>
     </Provider>
   );
